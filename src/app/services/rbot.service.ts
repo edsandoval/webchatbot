8ba3d609157a7838eb9de12346d062b63941cebf
dsandoval <sandoval.e.d@gmail.com>
2018-10-03 23:57:44 -0300

    Agregada entidad bot, para contener las distintas configuraciones de rasa.

--- src/app/services/rbot.service.ts
+++ src/app/services/rbot.service.ts
@@ -4,7 +4,10 @@ import 'rxjs/add/operator/map';
 
 @Injectable()
 export class RBotService {
-  private ID_BOT = 15248521;
+  // Identificador del bot, tiene que estar dado de alta en el middletier.
+  private BOT_ID = '5bb55dc4bb6d091bb4de6888';
+
+  // Url del middletier
   private rBotURL = 'http://localhost:8080/api/r-bot';
 
   constructor(private http: Http) {
@@ -12,8 +15,8 @@ export class RBotService {
 
   public getResponse(query: string) {
     const data = {
-      id: 1,
-      idBot: this.ID_BOT,
+      id: 1, // Identificador del cliente, TODO: deberia generarse para cada cliente disinto.
+      botId: this.BOT_ID,
       message: query,
       lang: 'es'
     };
